5a2617cd119ddbcafeb0375b3cb6044c15e16a49
DimitraK92 <79407886+DimitraK92@users.noreply.github.com>
2021-05-14 23:07:48 +0300

    Add files via upload

---
+++ MixUserInput/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the first word.");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Enter the second word.");
+            string secondWord = Console.ReadLine();           
+            
+            Console.WriteLine(MixWords(firstWord,secondWord));
+            Console.ReadKey();
+
+        }
+
+        private static string MixWords(string a, string b)
+        {
+            char[] aCharArray = a.ToCharArray();
+            char[] bCharArray = b.ToCharArray();
+
+            char[] aAndbArray = new char[aCharArray.Length + bCharArray.Length];
+            aAndbArray = a.Concat(b).ToArray();            
+
+            char[] newWordArray = new char[aAndbArray.Length];
+
+
+            Random rnd = new Random();
+
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < newWordArray.Length; i++)
+            {
+                
+                int index = rnd.Next(0, aAndbArray.Length);
+                while (numbers.Contains(index))
+                {
+                    index = rnd.Next(0, aAndbArray.Length);                   
+                }
+                if (!numbers.Contains(index))
+                {
+                    numbers.Add(index);
+                    newWordArray[i] = aAndbArray[index];
+                }
+
+            }
+            
+            return String.Concat(newWordArray);
+
+        }
+    }
+}
